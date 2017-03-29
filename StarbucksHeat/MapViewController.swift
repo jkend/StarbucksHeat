adbee788c76ee5c580b2b59eca3c3f98af420a22
jkend <jkend@users.noreply.github.com>
2017-03-29 18:48:12 -0400

    deleted extraneous commented-out code, added print statements

--- StarbucksHeat/MapViewController.swift
+++ StarbucksHeat/MapViewController.swift
@@ -21,9 +21,22 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         
             // Position and zoom the map to just fit the heatmap data on screen
             mapView.setVisibleMapRect(heatMap.boundingMapRect, animated: true)
-        
+            print("rectangle: \(heatMap.boundingMapRect)")
             // Add the heatmap overlay to the map view
             mapView.add(heatMap)
+ 
+            print("number found: \(heatPoints.count)")
+            // This would be unkind for this many points!!
+            /*
+            for heatPoint in heatPoints {
+                let annotation = MKPointAnnotation()
+                let mapPoint = heatPoint.mapPoint
+                let coord = MKCoordinateForMapPoint(mapPoint)
+                annotation.coordinate = coord
+                mapView.addAnnotation(annotation)
+
+            }
+             */
         }
     }
 
@@ -33,9 +46,9 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         return heatMapRenderer
     }
     
-    // Move this into a parser class
+    // Parsing really belongs elsewhere, but it's okay here for now.
     private func parseData() -> [HeatPoint]? {
-        let dataURL = Bundle.main.url(forResource:"xy4y-c4mk", withExtension: "json")
+        let dataURL = Bundle.main.url(forResource:"xy4y-c4mk-US", withExtension: "json")
         guard let fileData = NSData(contentsOf: dataURL!) else {
             return nil
         }
@@ -43,21 +56,17 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
         // We only care about the location of each store - that's what
         // the heap map will use.
-        //var pointDict: [NSValue:Int]
+
         var pointsArray =  [HeatPoint]()
         for store in allStores as! [Dictionary<String, Any>] {
             let coordinatesJSON = store["coordinates"] as! [String: Any]
             let latitude = coordinatesJSON["latitude"]
             let longitude = coordinatesJSON["longitude"]
             if let lat = Double(latitude as! String), let lng = Double(longitude as! String) {
-                // So this is kind of a pain. Make a CLLocationCoordinate2D of each latitude
-                // and longitude pair.  But in order to put that into a Dictionary, it has
-                // to be Hashable, so turn it into an NSValue of type mkCoordinate. Then
-                // make a Dictionary entry of this NSValue as the key and 1 as its value.
+
                 let location = CLLocationCoordinate2D(latitude: lat, longitude: lng)
                 let hp = HeatPoint(mapPoint: MKMapPointForCoordinate(location), heatValue: 1)
-                //pointDict = [NSValue(mkCoordinate: location) : 1]
-                //coordinatesDict[NSValue(mkCoordinate: location)] = 1
+
                 pointsArray.append(hp)
             }
         }
