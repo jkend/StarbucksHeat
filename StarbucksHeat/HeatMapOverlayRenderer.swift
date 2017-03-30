fb511b9975af1a38db3f8d8c8922c00de3c4d171
jkend <jkend@users.noreply.github.com>
2017-03-30 15:55:42 -0400

    first try at rendering the overlay

--- StarbucksHeat/HeatMapOverlayRenderer.swift
+++ StarbucksHeat/HeatMapOverlayRenderer.swift
@@ -11,11 +11,78 @@ import MapKit
 
 class HeatMapOverlayRenderer: MKOverlayRenderer {
     
+    
     override init(overlay: MKOverlay) {
         super.init(overlay: overlay)
+        scaleMe()
     }
     
     override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
+        let HeatRadiusInPoints = 48
+        let heatMapOverlay = self.overlay as! HeatMap
+        let mapPointsWithHeat = heatMapOverlay.mapPointsWithHeatIn(rect: mapRect, scale: zoomScale)
         
+        let screenRect = self.rect(for: mapRect)
+
+        let screenColumns = Int(screenRect.size.width * zoomScale)
+        let screenRows = Int(screenRect.size.height * zoomScale)
+        var heatValuesWithinRect = [Double](repeating: 0.0, count:screenColumns * screenRows)
+        
+        for heatPoint in mapPointsWithHeat {
+            if heatPoint.heatValue > 0 {
+                // Figure out where we are on the screen right now
+                let screenPoint = self.point(for: heatPoint.mapPoint)
+    
+                let matrixCoord = CGPoint(x: (screenPoint.x - screenRect.origin.x) * zoomScale, y:
+                                                  (screenPoint.y - screenRect.origin.y) * zoomScale)
+                
+                for i in 0..<(2 * HeatRadiusInPoints) {
+                    for j in 0..<(2 * HeatRadiusInPoints) {
+                        //find the array index
+                        let column = Int(matrixCoord.x - CGFloat(HeatRadiusInPoints + i))
+                        let row = Int(matrixCoord.y - CGFloat(HeatRadiusInPoints + j))
+                        
+                        // Check the bounds of the row and column
+                        if row >= 0 && column >= 0 && row < screenRows && column < screenColumns {
+                            let index = screenColumns * row + column
+                            heatValuesWithinRect[index] += heatPoint.heatValue //* _scaleMatrix[j * 2 * kSBHeatRadiusInPoints + i];
+                        }
+                
+                    }
+                }
+            }
+        }
+  
+        // Now, go through the array we just built and colorize based on value and zoom scale
+        for index in 0..<heatValuesWithinRect.count {
+            // Don't bother if the value here isn't greater than zero
+            if heatValuesWithinRect[index] > 0 {
+                let heatValue = heatValuesWithinRect[index]
+                
+                let (red, green, blue) = colorize(value: CGFloat(heatValue), between: 0.0, and: 1.0)
+
+                context.setFillColor(red: red, green: green, blue: blue, alpha: 0.5)
+                let column = index % screenColumns
+                let row = index / screenColumns
+                
+                // Re-scale to match current zoomScale and visible rectangle
+                let thisPointsScreenRect = CGRect(x: screenRect.origin.x + CGFloat(column) / zoomScale, y: screenRect.origin.y + CGFloat(row) / zoomScale, width: 1/zoomScale, height: 1/zoomScale)
+
+                context.fill(thisPointsScreenRect)
+            }
+        }
+    }
+
+    private func scaleMe() {
+        
+    }
+    
+    private func colorize(value: CGFloat, between minimum: CGFloat, and maximum: CGFloat) -> (CGFloat, CGFloat, CGFloat)
+    {
+        let valueRatio = 2 * (value - minimum) / (maximum - minimum)
+        let blue = max(0,  (1 - valueRatio))
+        let red = max(0, (valueRatio - 1))
+        let green = 1 - blue - red
+        return (red, green, blue)
     }
 }
