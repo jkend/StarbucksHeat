8decabb2e004eccb24f5cb06e133c1f58463eb7f
jkend <jkend@users.noreply.github.com>
2017-04-03 17:35:24 -0400

    Fixed the "moved over" bug Added method to help with scaling heat values wrt zoom level When colorizing the heat value, take its square root Lots of comments and cleanup

--- StarbucksHeat/HeatMapOverlayRenderer.swift
+++ StarbucksHeat/HeatMapOverlayRenderer.swift
@@ -11,43 +11,55 @@ import MapKit
 
 class HeatMapOverlayRenderer: MKOverlayRenderer {
     
+    lazy var scales: [Double] = self.setupScales()
     
-    override init(overlay: MKOverlay) {
-        super.init(overlay: overlay)
-        scaleMe()
-    }
+    let HeatRadiusInPoints = 48
+    let MapTileDimension = 256
     
-    override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
-        let HeatRadiusInPoints = 48
-        let heatMapOverlay = self.overlay as! HeatMap
-        let mapPointsWithHeat = heatMapOverlay.mapPointsWithHeatIn(rect: mapRect, scale: zoomScale)
-        
-        let screenRect = self.rect(for: mapRect)
+    override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext)
+    {
+        // Get our mapRect as a CGRect
+        let screenRect: CGRect = self.rect(for: mapRect)
 
-        let screenColumns = Int(screenRect.size.width * zoomScale)
-        let screenRows = Int(screenRect.size.height * zoomScale)
-        var heatValuesWithinRect = [Double](repeating: 0.0, count:screenColumns * screenRows)
+        // Because of the tiling the renderer does, values will be cut off unless we use a larger
+        // rectangle.  So ask for the heatvalues of a padded, "expanded" rectangle.
+        let paddingAmount: CGFloat = 128 / zoomScale
+        let expandedRect = CGRect(x: screenRect.origin.x - paddingAmount,
+                                  y: screenRect.origin.y - paddingAmount,
+                                  width: screenRect.size.width + (2 * paddingAmount),
+                                  height: screenRect.size.height + (2 * paddingAmount))
+
+        let expandedMapRect: MKMapRect = self.mapRect(for: expandedRect)
+        
+        // Ask the overlay for all the heat points in this padded rectangle
+        let heatMapOverlay = self.overlay as! HeatMap
+        let mapPointsWithHeat = heatMapOverlay.mapPointsWithHeatIn(rect: expandedMapRect, scale: zoomScale)
+
+        // This array will store the heat values based on scale within this fixed-sized tile
+        var heatValuesWithinRect = [Double](repeating: 0.0, count:MapTileDimension * MapTileDimension)
         
         for heatPoint in mapPointsWithHeat {
             if heatPoint.heatValue > 0 {
                 // Figure out where we are on the screen right now
                 let screenPoint = self.point(for: heatPoint.mapPoint)
     
-                let matrixCoord = CGPoint(x: (screenPoint.x - screenRect.origin.x) * zoomScale, y:
-                                                  (screenPoint.y - screenRect.origin.y) * zoomScale)
-                
-                for i in 0..<(2 * HeatRadiusInPoints) {
-                    for j in 0..<(2 * HeatRadiusInPoints) {
-                        //find the array index
-                        let column = Int(matrixCoord.x - CGFloat(HeatRadiusInPoints + i))
-                        let row = Int(matrixCoord.y - CGFloat(HeatRadiusInPoints + j))
-                        
+                let gridCoord = CGPoint(x: (screenPoint.x - screenRect.origin.x) * zoomScale,
+                                        y: (screenPoint.y - screenRect.origin.y) * zoomScale)
+  
+                for c in 0..<(2 * HeatRadiusInPoints) {
+                    for r in 0..<(2 * HeatRadiusInPoints) {
+                        // Check all the points surrounding this tile coordinate, and scale their
+                        // heat according to how close they are to this heat point.
+                        // Oh yeah, and when doing Swift's absurd type conversions, watch out
+                        // where you put your parentheses! Grrr.
+                        let column = Int((gridCoord.x - CGFloat(HeatRadiusInPoints) + CGFloat(c)).rounded(.down))
+                        let row =  Int((gridCoord.y - CGFloat(HeatRadiusInPoints) + CGFloat(r)).rounded(.down))
+
                         // Check the bounds of the row and column
-                        if row >= 0 && column >= 0 && row < screenRows && column < screenColumns {
-                            let index = screenColumns * row + column
-                            heatValuesWithinRect[index] += heatPoint.heatValue //* _scaleMatrix[j * 2 * kSBHeatRadiusInPoints + i];
+                        if row >= 0 && row < MapTileDimension && column >= 0 && column < MapTileDimension {
+                            let index = MapTileDimension * row + column
+                            heatValuesWithinRect[index] += heatPoint.heatValue * scales[r * 2 * HeatRadiusInPoints + c];
                         }
-                
                     }
                 }
             }
@@ -57,24 +69,48 @@ class HeatMapOverlayRenderer: MKOverlayRenderer {
         for index in 0..<heatValuesWithinRect.count {
             // Don't bother if the value here isn't greater than zero
             if heatValuesWithinRect[index] > 0 {
-                let heatValue = heatValuesWithinRect[index]
-                
-                let (red, green, blue) = colorize(value: CGFloat(heatValue), between: 0.0, and: 1.0)
-                let alpha = min(0.8, CGFloat(heatValue))
+                // Colorize this heat value. For now don't do anything fancy with the alpha.
+                let alpha: CGFloat = 0.6
+                let (red, green, blue) = colorize(value: CGFloat(heatValuesWithinRect[index]),
+                                                  between: 0.0,
+                                                  and: 1.0)
                 context.setFillColor(red: red, green: green, blue: blue, alpha: alpha)
-                let column = index % screenColumns
-                let row = index / screenColumns
+                
+                // Now figure out where this color belongs on screen
+                let tileColumn = index % MapTileDimension
+                let tileRow = index / MapTileDimension
                 
                 // Re-scale to match current zoomScale and visible rectangle
-                let thisPointsScreenRect = CGRect(x: screenRect.origin.x + CGFloat(column) / zoomScale, y: screenRect.origin.y + CGFloat(row) / zoomScale, width: 1/zoomScale, height: 1/zoomScale)
+                let thisPointsScreenRect = CGRect(x: screenRect.origin.x + CGFloat(tileColumn) / zoomScale,
+                                                  y: screenRect.origin.y + CGFloat(tileRow) / zoomScale,
+                                                  width: 1/zoomScale,
+                                                  height: 1/zoomScale)
 
                 context.fill(thisPointsScreenRect)
             }
         }
     }
 
-    private func scaleMe() {
+    // I got this scaling matrix from Ryan Olson's HeatMap project, it really helped!
+    private func setupScales() -> [Double] {
+        var matrix = [Double](repeating: 0.0, count:2 * HeatRadiusInPoints * 2 * HeatRadiusInPoints)
         
+        for c in 0..<2 * HeatRadiusInPoints {
+            for r in 0..<2 * HeatRadiusInPoints {
+                let distanceSquared = (c - HeatRadiusInPoints) * (c - HeatRadiusInPoints) + (r - HeatRadiusInPoints) * (r - HeatRadiusInPoints)
+                let distance = sqrt(Double(distanceSquared))
+                
+                var scalingFactor = 1 - (distance / Double(HeatRadiusInPoints))
+                if scalingFactor < 0 {
+                    scalingFactor = 0
+                } else {
+                    scalingFactor = pow(2, (-distance/10.0)) - pow(2, Double(-HeatRadiusInPoints)/10)
+                }
+                matrix[r * 2 * HeatRadiusInPoints + c] = scalingFactor
+                
+            }
+        }
+        return matrix
     }
     
     // Note: Methods of colorizing are many, and I liked this one best - it's one that MATLAB has used for some time,
@@ -85,18 +121,20 @@ class HeatMapOverlayRenderer: MKOverlayRenderer {
         var green: CGFloat
         var blue: CGFloat
         
-        if value <= 0 {
+        let adjustedValue = sqrt(value)
+        
+        if adjustedValue <= 0 {
             (red, green,blue) = (0.0, 0.0, 0.0)
-        } else if value < 0.125 {
-            (red, green,blue) = (0.0, 0.0, 4 * (value + 0.125))
-        } else if value < 0.375 {
-            (red, green,blue) = (0.0, 4 * (value - 0.125), 1.0)
-        } else if value < 0.625 {
-            (red, green,blue) = (4 * (value - 0.375), 1.0, 1 - 4 * (value - 0.375))
-        } else if value < 0.875 {
-            (red, green,blue) = (1.0, 1 - 4 * (value - 0.625), 0.0)
+        } else if adjustedValue < 0.125 {
+            (red, green,blue) = (0.0, 0.0, 4 * (adjustedValue + 0.125))
+        } else if adjustedValue < 0.375 {
+            (red, green,blue) = (0.0, 4 * (adjustedValue - 0.125), 1.0)
+        } else if adjustedValue < 0.625 {
+            (red, green,blue) = (4 * (adjustedValue - 0.375), 1.0, 1 - 4 * (adjustedValue - 0.375))
+        } else if adjustedValue < 0.875 {
+            (red, green,blue) = (1.0, 1 - 4 * (adjustedValue - 0.625), 0.0)
         } else {
-            (red, green,blue) = (max(1 - 4 * (value - 0.875), 0.5), 0.0, 0.0)
+            (red, green,blue) = (max(1 - 4 * (adjustedValue - 0.875), 0.5), 0.0, 0.0)
 
         }
         return (red, green, blue)
